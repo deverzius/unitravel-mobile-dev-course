d0f05f8c57a6f51552504f9303f4270924c60190
BuiThanhBinh <binh.s078251@gmail.com>
2024-01-01 23:25:25 +0700

    fix homescreen

--- src/Components/Swiper/index.tsx
+++ src/Components/Swiper/index.tsx
@@ -13,45 +13,48 @@ export function SwiperComponent(props: any) {
   const [token, setToken] = useState<String>('');
   const [isLoading, setIsLoading] = useState(true);
   const [viewData, setViewData] = useState<Location[]>([]);
-  const [getLocations, {isLoading: getLocationIsLoading}] = useLazyGetLocationsQuery();
-  const [getFavoriteLocations, {isLoading: getFavoriteLocationsIsLoading}] = useLazyGetFavoriteLocationsQuery();
-  const [getRecentlyLocations, {isLoading: getRecentlyLocationsIsLoading}] = useLazyGetRecentlyLocationsQuery();
-  const [getRecommendedLocations, {isLoading: getRecommendedLocationsIsLoading}] = useLazyGetRecommendedLocationsQuery();
+  const [getLocations, {isSuccess: getLocationIsLoading}] = useLazyGetLocationsQuery();
+  const [getFavoriteLocations, {isSuccess: getFavoriteLocationsIsLoading}] = useLazyGetFavoriteLocationsQuery();
+  const [getRecentlyLocations, {isSuccess: getRecentlyLocationsIsLoading}] = useLazyGetRecentlyLocationsQuery();
+  const [getRecommendedLocations, {isSuccess: getRecommendedLocationsIsLoading}] = useLazyGetRecommendedLocationsQuery();
 
   useEffect(() => {
     const fetchData = async () => {
+      
       Promise.resolve(AsyncStorage.getItem('token'))
       .then(result => {
         if (result) setToken(result)
         else setToken('eyJhbGciOiJIUzI1NiIsImtpZCI6IjVuRURsaDJ3aHJ1dVhRemQiLCJ0eXAiOiJKV1QifQ.eyJhdWQiOiJhdXRoZW50aWNhdGVkIiwiZXhwIjoxNzA0MTExOTYxLCJpYXQiOjE3MDQxMDgzNjEsImlzcyI6Imh0dHBzOi8vZ29idWlnZGF3aHZhbHl2eXhoeGYuc3VwYWJhc2UuY28vYXV0aC92MSIsInN1YiI6IjIxMTMxNjc0LTllNTktNGE3OC1hNGMyLThjZTc3Y2EwY2QxNyIsImVtYWlsIjoiZGVsdG9yYTFzdEBnbWFpbC5jb20iLCJwaG9uZSI6IiIsImFwcF9tZXRhZGF0YSI6eyJwcm92aWRlciI6ImVtYWlsIiwicHJvdmlkZXJzIjpbImVtYWlsIl19LCJ1c2VyX21ldGFkYXRhIjp7fSwicm9sZSI6ImF1dGhlbnRpY2F0ZWQiLCJhYWwiOiJhYWwxIiwiYW1yIjpbeyJtZXRob2QiOiJwYXNzd29yZCIsInRpbWVzdGFtcCI6MTcwNDEwODM2MX1dLCJzZXNzaW9uX2lkIjoiZTQ0MGE5ODMtYTBiNi00NzcxLWI3ZDEtN2Q2N2U4NjZhOWZhIn0.lRwDMvOz3nDRDGqk6IakIwgPVWyPZHltTayY6ygDl74')
       })
 
-      const result1 = await getLocations( { token } )
-      const result2 = await getFavoriteLocations( { token } )
-      const result3 = await getRecentlyLocations( { token } )
-      const result4 = await getRecommendedLocations( { token } )
-
       if(props.route.name == "Tất cả") {
-        if(result1.data?.data != undefined) setViewData(result1.data?.data)
-        setIsLoading(getLocationIsLoading)
+        const result = await getLocations( { token } )
+        if(result.data?.data != undefined) setViewData(result.data?.data)
+        setIsLoading(!getLocationIsLoading)
       } else if(props.route.name == "Nổi bật") {
-        if(result2.data?.data != undefined) setViewData(result2.data?.data)
-        setIsLoading(getFavoriteLocationsIsLoading)
+        const result = await getFavoriteLocations( { token } )
+        if(result.data?.data != undefined) setViewData(result.data?.data)
+        setIsLoading(!getFavoriteLocationsIsLoading)
       } else if(props.route.name == "Gần đây") {
-        if(result3.data?.data != undefined) setViewData(result3.data?.data)
-        setIsLoading(getRecentlyLocationsIsLoading)
+        const result = await getRecentlyLocations( { token } )
+        if(result.data?.data != undefined) setViewData(result.data?.data)
+        setIsLoading(!getRecentlyLocationsIsLoading)
       } else if(props.route.name == "Đề xuất") {
-        if(result4.data?.data != undefined) setViewData(result4.data?.data)
-        setIsLoading(getRecommendedLocationsIsLoading)
+        const result = await getRecommendedLocations( { token } )
+        if(result.data?.data != undefined) setViewData(result.data?.data)
+        setIsLoading(!getRecommendedLocationsIsLoading)
       }
     }
     fetchData()
-  }, [props.route.name])
+  }, [getLocationIsLoading, getFavoriteLocationsIsLoading, getRecentlyLocationsIsLoading, getRecommendedLocationsIsLoading])
   
   const page = viewData.map((item) => {        
     return (
-      <TouchableHighlight underlayColor={Colors.INDIGO2} key={item.id} onPress={() => navigation.push(RootScreens.MAIN)}>
-      <View style={styles.wrapper}>
+      <TouchableHighlight underlayColor={Colors.INDIGO2}
+        key={item.id}
+        onPress={() => navigation(RootScreens.MAIN, {id: item.id})}
+      >
+      <View style={styles.wrapper1}>
         <Image style={styles.imageSwiper} source={{uri: item.imageUrl}} />
         <Text style={styles.text1}>{item.name}</Text>
         <View style={styles.textWrap}>
@@ -69,7 +72,10 @@ export function SwiperComponent(props: any) {
 
   const list = viewData.map((item) => {        
     return (
-      <TouchableHighlight underlayColor={Colors.INDIGO2} key={item.id} onPress={() => navigation.push(RootScreens.MAIN)}>
+      <TouchableHighlight underlayColor={Colors.INDIGO2}
+        key={item.id}
+        onPress={() => navigation(RootScreens.MAIN, {id: item.id})}
+      >
         <View style={styles.scrollItem}>
           <Image style={styles.imageScroll} source={{uri: item.imageUrl}} />
           <View style={styles.scrollWrap}>
@@ -90,12 +96,12 @@ export function SwiperComponent(props: any) {
 
   return (
     <View style={styles.container}>
-      <Loader />
+      {isLoading && <Loader />}
       <ScrollView>
       <View style={styles.wrapper}>
-        <Swiper  loop={true} showsButtons={true} autoplay={true} dot={<View style={{}}/>} activeDot={<View style={{}}/>} >
+        <ScrollView horizontal={true}>
           {page}
-        </Swiper>
+        </ScrollView>
       </View>
       <View style={styles.scroll}>
         <Text style={styles.title}>{props.route.name}</Text>
@@ -114,6 +120,21 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
   },
   wrapper: {
+    display: 'flex',
+    flexDirection: 'row',
+    paddingTop: 7,
+    paddingLeft: 15,
+    paddingRight: 15,
+    paddingBottom: 7,
+    height: 220,
+    textAlign: 'center',
+    borderBottomWidth: 2,
+    borderBottomColor: '#D2D2D2',
+  },
+  wrapper1: {
+    display: 'flex',
+    flexDirection: 'column',
+    flexWrap: 'wrap',
     paddingTop: 7,
     paddingLeft: 15,
     paddingRight: 15,
@@ -124,7 +145,7 @@ const styles = StyleSheet.create({
     borderBottomColor: '#D2D2D2',
   },
   imageSwiper: {
-    width: 200,
+    width: 180,
     height: 120,
     borderRadius: 5,
     alignSelf: 'center'
