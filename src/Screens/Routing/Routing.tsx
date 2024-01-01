5e8987780f118c80b2b2628fb1f938c9a2ef9082
deverzius <nguyenmanh.4949@gmail.com>
2024-01-01 15:30:19 +0700

    update: add map input

--- src/Screens/Routing/Routing.tsx
+++ src/Screens/Routing/Routing.tsx
@@ -7,6 +7,11 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Loader } from '@/Components/Loader';
 import MapView, { Marker, Polyline } from 'react-native-maps'
 import * as Location from 'expo-location';
+import { PrimaryButton } from '@/Components/Button';
+import { BasicInput } from '@/Components/Input';
+import { Colors, FontSize } from '@/Theme/Variables';
+import CusHeader from '@/Components/CusHeader';
+import CusText from '@/Components/CusText';
 
 
 export interface IRoutingProps {
@@ -18,16 +23,9 @@ export const Routing = (props: IRoutingProps) => {
   const { navigation } = props;
   const [location, setLocation] = React.useState<any>(false);
   const [region, setRegion] = React.useState<any>(false);
+  const [departure, setDeparture] = React.useState<any>("");
+  const [destination, setDestination] = React.useState<any>("");
 
-  // const [
-  //   getNotis,
-  //   {
-  //     data: notisData,
-  //     isSuccess: notisSuccess,
-  //     isLoading: notisLoading,
-  //     error: notisError,
-  //   }
-  // ] = useGetNotisMutation();
   const getLocation = async () => {
     let { status } = await Location.requestForegroundPermissionsAsync();
     if (status !== 'granted') {
@@ -47,8 +45,16 @@ export const Routing = (props: IRoutingProps) => {
     longitudeDelta: 0.0421,
   };
 
-  const onRegionChange = (region: any) => { 
+  const onRegionChange = (region: any) => {
     setRegion(region);
+  }
+
+  const handleSetDeparture = (text: string) => {
+    setDeparture(text);
+  }
+
+  const handleSetDestination = (text: string) => {
+    setDestination(text);
   }
 
   return (
@@ -68,12 +74,39 @@ export const Routing = (props: IRoutingProps) => {
             }}
             />
           }
+          <Polyline
+            coordinates={[
+              {
+                latitude: 10.8768353,
+                longitude: 106.8093998
+              },
+            ]}
+            strokeColor="#000" // fallback for when `strokeColors` is not supported by the map-provider
+            strokeColors={[
+              '#7F0000',
+              '#00000000', // no color, creates a "long" gradient between the previous and next coordinate
+              '#B24112',
+              '#E5845C',
+              '#238C23',
+              '#7F0000',
+            ]}
+            strokeWidth={6}
+          />
         </MapView>
         <View style={styles.control}>
-          <Text>Tìm Đường Đi</Text>
-          <Button onPress={getLocation}>
-            <Text>Điểm Đến</Text>
-          </Button>
+          <CusText style={styles.heading}>Tìm Đường Đi</CusText>
+
+          <BasicInput
+            placeholder="Nhập điểm đi"
+            onChangeText={handleSetDeparture}
+            value={departure}
+          />
+          <BasicInput
+            placeholder="Nhập điểm đến"
+            onChangeText={handleSetDestination}
+            value={destination}
+          />
+          <PrimaryButton title="Tìm đường" onPress={getLocation} style={{ width: 190 }} />
         </View>
       </>
     </View>
@@ -88,7 +121,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   heading: {
-
+    fontFamily: 'montExtraBold',
+    color: Colors.INDIGO5,
+    fontSize: 24,
+    paddingTop: 40,
+    paddingBottom: 24,
   },
   map: {
     width: "100%",
@@ -96,9 +133,10 @@ const styles = StyleSheet.create({
   },
   control: {
     position: 'absolute',
-    backgroundColor: "red",
-    top: "64%",
-    width: "104%",
+    alignItems: 'center',
+    backgroundColor: Colors.WHITE,
+    top: "60%",
+    width: "102%",
     paddingHorizontal: "20%",
     height: "100%",
     borderRadius: 36,
